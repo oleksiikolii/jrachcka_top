2105fe4db3ce0b8ec70c5aee08c71c175baa4ac4
oleksiikolii <igravkompik@gmail.com>
2023-09-21 12:59:29 +0300

    fix choices

--- jrachcka_app/models.py
+++ jrachcka_app/models.py
@@ -1,9 +1,7 @@
-from enum import Enum
-
 from django.db import models
 
 
-class CheckType(Enum):
+class CheckType(models.TextChoices):
     KITCHEN = "kitchen"
     CLIENT = "client"
 
@@ -11,17 +9,17 @@ class CheckType(Enum):
 class Printer(models.Model):
     name = models.CharField(max_length=127)
     api_key = models.CharField(max_length=255, unique=True)
-    check_type = models.CharField(max_length=127, choices=CheckType)
+    check_type = models.CharField(max_length=127, choices=CheckType.choices)
 
 
 class Check(models.Model):
-    class StatusType(Enum):
+    class StatusType(models.TextChoices):
         NEW = "new"
         RENDERED = "rendered"
         PRINTED = "printed"
 
     printer_id = models.ForeignKey(to=Printer, on_delete=models.CASCADE)
-    type = models.CharField(max_length=127, choices=CheckType)
+    type = models.CharField(max_length=127, choices=CheckType.choices)
     order = models.JSONField()
-    status = models.CharField(max_length=127, choices=StatusType)
+    status = models.CharField(max_length=127, choices=StatusType.choices)
     pdf_file = models.FileField()
